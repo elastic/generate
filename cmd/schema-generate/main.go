fc53b288d99830aa42e1fe4a2583e9abde63cc3b
Aleksandr Maus <aleksandr.maus@elastic.co>
2020-12-01 09:35:43 -0500

    Update to allow to switch between Go structs and ES mapping generation

--- cmd/schema-generate/main.go
+++ cmd/schema-generate/main.go
@@ -11,11 +11,16 @@ import (
 	"github.com/aleksmaus/generate"
 )
 
+const (
+	modeES = "es"
+)
+
 var (
 	o                     = flag.String("o", "", "The output file for the schema.")
 	p                     = flag.String("p", "main", "The package that the structs are created in.")
 	i                     = flag.String("i", "", "A single file path (used for backwards compatibility).")
 	schemaKeyRequiredFlag = flag.Bool("schemaKeyRequired", false, "Allow input files with no $schema key.")
+	mode                  = flag.String("m", "", "Output mode: Default (empty) for Go structures or \"es\" for ES mapping")
 )
 
 func main() {
@@ -63,5 +68,10 @@ func main() {
 		}
 	}
 
-	generate.Output(w, g, *p)
+	switch *mode {
+	case modeES:
+		generate.ESOutput(w, g, *p)
+	default:
+		generate.Output(w, g, *p)
+	}
 }
